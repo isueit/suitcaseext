d07b2ebdf830d2d16c460de77dfdc155446c9ef9
rlfrahm <frahmryan@gmail.com>
2014-11-13 16:13:00 -0600

    Adding close icon and hover state to filter button

--- js/suitcase_block_search.js
+++ js/suitcase_block_search.js
@@ -11,7 +11,7 @@
         var length_requirement = 7;
         $('.block .item-list').each(function() {
           if($(this).find('> ul > li').length > length_requirement) {
-            var $e = $('<div class="block-search-input"><input type="text" value="" placeholder="Filter items" style="display: none"><div class="pull-down"><span></span></div><div class="clear-search" style="display: none"><span>x</span></div></div>');
+            var $e = $('<div class="block-search-input"><input type="text" value="" placeholder="Filter items" style="display: none"><div class="pull-down"><span class="icon-filter"></span></div><div class="clear-search" style="display: none"><span>x</span></div></div>');
             $e.find('input').keyup(function(e) {
               if($(this).val() == '') {
                 $(this).parent().parent().find('li').show();
@@ -31,6 +31,10 @@
         });
         $('.block-search-input .pull-down').click(function() {
           $(this).parent().find('input').toggle();
+          $(this).find('span').toggleClass('icon-filter').toggleClass('icon-close');
+          if($(this).find('span').hasClass('icon-close')) {
+            $(this).parent().find('input').focus();
+          }
         });
       });
     }
