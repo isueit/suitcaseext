97dafc52667a7b716b901e469505a3f0e79beb41
rlfrahm <frahmryan@gmail.com>
2014-11-13 14:41:45 -0600

    Added button to drop down filter

--- js/suitcase_block_search.js
+++ js/suitcase_block_search.js
@@ -8,13 +8,29 @@
   Drupal.behaviors.suitcaseBlockSearch = {
     attach: function (context) {
       $('body', context).once('suitcaseBlockSearch', function() {
+        var length_requirement = 7;
         $('.block .item-list').each(function() {
-          var $e = $('<div class="block-search"><input type="text" value=""></div>');
-          $e.find('input').keyup(function(e) {
-            $(this).parent().parent().find('li:not(:contains('+$(this).val()+'))').hide();
-            $(this).parent().parent().find('li:contains('+$(this).val()+')').show();
-          });
-          $(this).prepend($e);
+          if($(this).find('> ul > li').length > length_requirement) {
+            var $e = $('<div class="block-search-input"><input type="text" value="" placeholder="Filter items" style="display: none"><div class="pull-down"><span>...</span></div><div class="clear-search" style="display: none"><span>x</span></div></div>');
+            $e.find('input').keyup(function(e) {
+              if($(this).val() == '') {
+                $(this).parent().parent().find('li').show();
+                $(this).parent().find('.clear-search').hide();
+              } else {
+                $(this).parent().find('.clear-search').show();
+                $(this).parent().parent().find('li:not(:contains('+$(this).val()+'))').hide();
+                $(this).parent().parent().find('li:contains('+$(this).val()+')').show();
+              }
+            });
+            $(this).prepend($e);
+          }
+        });
+        $('.block-search-input .clear-search').click(function(e) {
+          $(this).parent().find('input').val('').keyup();
+          $(this).hide();
+        });
+        $('.block-search-input .pull-down').click(function() {
+          $(this).parent().find('input').toggle();
         });
       });
     }
